0d6a207662c8740ae318b191be29f1505c32463c
carlo <carlo@DESKTOP-H110MH>
2023-08-27 21:30:55 -0300

    Add project files.

---
+++ Trabalho M1 termo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "dicionario.hpp"
+
+
+
+int main()
+{
+    srand(time(NULL));
+
+    string p1 = lista[rand() % TAM_LISTA];
+    string p2 = lista[rand() % TAM_LISTA];
+    string p3 = lista[rand() % TAM_LISTA];
+    cout << "Palavra 1: " << p1 << endl;
+    cout << "Palavra 2: " << p2 << endl;
+    cout << "Palavra 3: " << p3 << endl;
+
+
+    return 0;
+}
