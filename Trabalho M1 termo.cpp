e57428dc01082bb844c44dbebf2fbfb4fefe0edd
Carlos Serena <7908953+CarlosA22@users.noreply.github.com>
2023-08-29 17:19:12 -0300

    deu ruim

--- Trabalho M1 termo.cpp
+++ Trabalho M1 termo.cpp
@@ -11,6 +11,122 @@
 #include <time.h> //para gerar numeros aleatorios
 #include "dicionario.hpp" //inclui o arquivo dicionario.hpp
 
+int tentativas(int NUM_PALAVRAS = 1) //função que define a quantidade de tentativas de acordo com a quantidade de palavras escolhidas pelo jogador
+{
+    //static int vet1[1], vet2[1], vet3[1], vet4[1], vet5[1], vet6[1], vet7[1], vet8[1], vet9[1];
+    static int mapa = 0;
+    static int tentativas = 0;
+
+    if (NUM_PALAVRAS == 1) //se o jogador escolher 1 palavra, ele terá 7 tentativas
+    {
+        tentativas = 7;
+    }
+    else if (NUM_PALAVRAS == 2) //se o jogador escolher 2 palavras, ele terá 7 tentativas
+    {
+        tentativas = 7;
+    }
+    else if (NUM_PALAVRAS == 3) //se o jogador escolher 3 palavras, ele terá 8 tentativas
+    {
+        tentativas = 8;
+    }
+    else if (NUM_PALAVRAS == 4) //se o jogador escolher 4 palavras, ele terá 9 tentativas
+    {
+        tentativas = 9;
+    }
+    else
+    {
+        system("cls");
+        cout << "Opção inválida. Tente novamente." << endl;
+    }
+
+
+    return tentativas;
+}
+
+
+void enquantoJoga(int qntPalavras, int qntPalavras2)
+{
+    static string vet1[5], vet2[5], vet3[5], vet4[5], vet5[5], vet6[5], vet7[5], vet8[5], vet9[5];
+    int qntTentativas = 0;
+
+    string p1 = lista[rand() % TAM_LISTA]; //sorteia uma palavra da lista
+    string p2 = lista[rand() % TAM_LISTA];
+    string p3 = lista[rand() % TAM_LISTA];
+    string p4 = lista[rand() % TAM_LISTA];
+
+    qntTentativas = qntPalavras;
+    tentativas(qntPalavras); //chama a função tentativas para definir a quantidade de tentativas de acordo com a quantidade de palavras escolhidas pelo jogador
+
+
+    if (qntTentativas == 7 and qntPalavras2 == 1) //se o jogador escolher 1 palavra, ele terá 7 tentativas
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            cout << "digite uma palavra: ";
+            cin >> vet1[i];
+            if (vet1[i] == p1)
+            {
+                cout << "voce acertou";
+            }
+            else
+            {
+                cout << "voce errou";
+            }
+        }
+
+    }
+    else  if (qntTentativas == 7 and qntPalavras2 == 2) //se o jogador escolher 2 palavras, ele terá 7 tentativas
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            cout << "digite uma palavra: ";
+            cin >> vet1[i];
+            if (vet1[i] == p1 or vet1[i] == p2)
+            {
+                cout << "voce acertou";
+            }
+            else
+            {
+                cout << "voce errou";
+            }
+        }
+
+    }
+    else if (qntTentativas == 8 and qntPalavras2 == 3) //se o jogador escolher 3 palavras, ele terá 8 tentativas
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            cout << "digite uma palavra: ";
+            cin >> vet1[i];
+            if (vet1[i] == p1)
+            {
+                cout << "voce acertou";
+            }
+            else
+            {
+                cout << "voce errou";
+            }
+        }
+    }
+    else if (qntTentativas == 9 and qntPalavras2 == 4) // se o jogador escolher 4 palavras, ele terá 9 tentativas
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            cout << "digite uma palavra: ";
+            cin >> vet1[i];
+            if (vet1[i] == p1)
+            {
+                cout << "voce acertou";
+            }
+            else
+            {
+                cout << "voce errou";
+            }
+        }
+    }
+}
+
+
 void menuWordle()
 {
     int qntPalavras = 0;
@@ -60,121 +176,6 @@ void menuWordle()
 }
 
 
-int tentativas(int NUM_PALAVRAS = 1) //função que define a quantidade de tentativas de acordo com a quantidade de palavras escolhidas pelo jogador
-{
-    //static int vet1[1], vet2[1], vet3[1], vet4[1], vet5[1], vet6[1], vet7[1], vet8[1], vet9[1];
-    static int mapa = 0;
-    static int tentativas = 0;
-
-    if (NUM_PALAVRAS == 1) //se o jogador escolher 1 palavra, ele terá 7 tentativas
-    {
-		tentativas = 7;
-	}
-    else if (NUM_PALAVRAS == 2) //se o jogador escolher 2 palavras, ele terá 7 tentativas
-    {
-		tentativas = 7;
-	}
-    else if (NUM_PALAVRAS == 3) //se o jogador escolher 3 palavras, ele terá 8 tentativas
-    {
-		tentativas = 8;
-	}
-    else if (NUM_PALAVRAS == 4) //se o jogador escolher 4 palavras, ele terá 9 tentativas
-    {
-		tentativas = 9;
-	}
-    else
-    {
-        system("cls");
-		cout << "Opção inválida. Tente novamente." << endl;
-	}
-
-
-    return tentativas;
-}
-
-
-int enquantoJoga(int qntPalavras, int qntPalavras2) 
-{
-    static string vet1[5], vet2[5], vet3[5], vet4[5], vet5[5], vet6[5], vet7[5], vet8[5], vet9[5];
-    int qntTentativas = 0;
-
-    string p1 = lista[rand() % TAM_LISTA]; //sorteia uma palavra da lista
-    string p2 = lista[rand() % TAM_LISTA]; 
-    string p3 = lista[rand() % TAM_LISTA];
-    string p4 = lista[rand() % TAM_LISTA];
-
-    qntTentativas = qntPalavras; 
-    tentativas(qntPalavras); //chama a função tentativas para definir a quantidade de tentativas de acordo com a quantidade de palavras escolhidas pelo jogador
-
-
-    if (qntTentativas == 7 and qntPalavras2 == 1) //se o jogador escolher 1 palavra, ele terá 7 tentativas
-    {
-        for (int i = 0; i < 7; i++)
-        {
-            cout << "digite uma palavra: ";
-			cin >> vet1[i];
-            if (vet1[i] == p1)
-            {
-                cout << "voce acertou";
-            }
-            else
-            {
-                cout << "voce errou";
-            }
-		}
-
-    }
-    else  if (qntTentativas == 7 and qntPalavras2 == 2) //se o jogador escolher 2 palavras, ele terá 7 tentativas
-    {
-        for (int i = 0; i < 7; i++)
-        {
-            cout << "digite uma palavra: ";
-            cin >> vet1[i];
-            if (vet1[i] == p1 or vet1[i] == p2)
-            {
-                cout << "voce acertou";
-            }
-            else
-            {
-                cout << "voce errou";
-            }
-        }
-
-    }
-    else if (qntTentativas == 8 and qntPalavras2 == 3) //se o jogador escolher 3 palavras, ele terá 8 tentativas
-    {
-        for (int i = 0; i < 8; i++)
-        {
-			cout << "digite uma palavra: ";
-            cin >> vet1[i];
-            if (vet1[i] == p1)
-            {
-				cout << "voce acertou";
-			}
-            else
-            {
-				cout << "voce errou";
-			}
-        }
-    }
-    else if (qntTentativas == 9 and qntPalavras2 == 4) // se o jogador escolher 4 palavras, ele terá 9 tentativas
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            cout<<"digite uma palavra: ";
-            cin >> vet1[i];
-            if (vet1[i] == p1) 
-            {
-				cout << "voce acertou";
-			}
-            else
-            {
-				cout << "voce errou";
-			}
-        }
-    }
-    
-}
 
 int main()
 {
